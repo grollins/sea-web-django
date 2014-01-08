1ca3d41798527b1f20d37f154c7665dfe4d7fb2b
Geoff Rollins <grollins@gmail.com>
2014-01-08 08:46:23 -0800

    Remove sites to fix a weird postgres-related error when connecting to most of the urls.

--- seaweb_project/seaweb_project/settings/base.py
+++ seaweb_project/seaweb_project/settings/base.py
@@ -185,7 +185,6 @@ DJANGO_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
